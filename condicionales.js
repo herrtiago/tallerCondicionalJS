f6d06e7e9c7ffa422cb2d4547efcdb3d67fb7998
Santi <herrtiago22@gmail.com>
2022-12-05 11:28:04 -0500

    9/9 ejercicios terminados

--- condicionales.js
+++ condicionales.js
@@ -1,81 +1,80 @@
 
+let n1 =+prompt("Ingrese numero 1"), n2=+prompt("Ingrese numero 2")
+if(n1>n2){
+    alert(`${n2},${n1}`)
+}else
+    alert(`${n1},${n2}`)
 
-// let n1 =+prompt("Ingrese numero 1"), n2=+prompt("Ingrese numero 2")
-// if(n1>n2){
-//     alert(`${n2},${n1}`)
-// }else
-//     alert(`${n1},${n2}`)
-
-// let numero = parseInt(prompt("Ingrese numero entre 1 y 10"))
-// if(numero>0 && numero<11){
-//     switch(numero){
-//         case 1:
-//         case 3:
-//         case 5:
-//         case 7:
-//         case 9:
-//             alert(`impar`)
-//             break
-//         case 2:
-//         case 4:
-//         case 6:
-//         case 8:
-//         case 10:
-//             alert(`par`)
-//             break
-//     }
-// }else
-//     alert(`${numero} no se encuentra entre 1 y 10`)
+let numero = parseInt(prompt("Ingrese numero entre 1 y 10"))
+if(numero>0 && numero<11){
+    switch(numero){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 9:
+            alert(`impar`)
+            break
+        case 2:
+        case 4:
+        case 6:
+        case 8:
+        case 10:
+            alert(`par`)
+            break
+    }
+}else
+    alert(`${numero} no se encuentra entre 1 y 10`)
 
 
-// let numero3 =parseInt(prompt("Ingrese un numero entre 1 y 10"))
-// if(numero3>0 && numero3<11){
-//     switch(numero3){
-//         case 1:
-//             alert(`${numero3} = uno`)
-//             break
-//         case 2:
-//             alert(`${numero3} = dos`)
-//             break
-//         case 3:
-//             alert(`${numero3} = tres`)
-//             break
-//         case 4:
-//             alert(`${numero3} = cuatro`)
-//             break
-//         case 5:
-//             alert(`${numero3} = cinco`)
-//             break
-//         case 6:
-//             alert(`${numero3} = seis`)
-//             break
-//         case 7:
-//             alert(`${numero3} = siete`)
-//             break
-//         case 8:
-//             alert(`${numero3} = ocho`)
-//             break
-//         case 9:
-//             alert(`${numero3} = nueve`)
-//             break
-//         case 10:
-//             alert(`${numero3} = diez`)
-//             break
-//     }
-// }else
-//     alert(`${numero3} no se encuentra entre 1 y 10`)
-
+let numero3 =parseInt(prompt("Ingrese un numero entre 1 y 10"))
+if(numero3>0 && numero3<11){
+    switch(numero3){
+        case 1:
+            alert(`${numero3} = uno`)
+            break
+        case 2:
+            alert(`${numero3} = dos`)
+            break
+        case 3:
+            alert(`${numero3} = tres`)
+            break
+        case 4:
+            alert(`${numero3} = cuatro`)
+            break
+        case 5:
+            alert(`${numero3} = cinco`)
+            break
+        case 6:
+            alert(`${numero3} = seis`)
+            break
+        case 7:
+            alert(`${numero3} = siete`)
+            break
+        case 8:
+            alert(`${numero3} = ocho`)
+            break
+        case 9:
+            alert(`${numero3} = nueve`)
+            break
+        case 10:
+            alert(`${numero3} = diez`)
+            break
+    }
+}else
+    alert(`${numero3} no se encuentra entre 1 y 10`)
 
 
 
 
-// let duracion =+ Math.round(prompt("Duracion llamada en minutos")),coste = 0
-// if(duracion<=3){
-//     coste = 200
-// }else{
-//     coste = 200 +((duracion-3)*100)
-// }
-// alert(`Valor de la llamada: $${coste}`)
+
+let duracion =+ Math.round(prompt("Duracion llamada en minutos")),coste = 0
+if(duracion<=3){
+    coste = 200
+}else{
+    coste = 200 +((duracion-3)*100)
+}
+alert(`Valor de la llamada: $${coste}`)
 
 
 
@@ -97,6 +96,8 @@ if(y<=c1 && x<=c2){
 
 
 
+
+
 let previo1,previo2,previo3,trabajo1,trabajo2,final,minima = 1,maxima=5
 previo1 =+prompt("Ingrese la nota del previo 1")
 previo2 =+prompt("Ingrese la nota del previo 2")
@@ -110,3 +111,45 @@ if(previo1 >=minima && previo1 <=maxima &&previo2 >=minima && previo2 <=maxima &
     alert(`Nota final ${final}`)
 }else
 alert("Notas por fuera de rango")
+
+
+
+
+let precioFinal,nombreArticulo = prompt("Ingrese nombre del articulo"),clave =+prompt("Ingrese la clave (1) o (2)"), precioOriginal =+prompt("Ingrese precio original"),cantidad=+prompt("Ingrese la cantidad"),descuento=0
+if(clave == 1)
+{
+    descuento = 0.1
+}
+else if(clave == 2)
+{
+    descuento = 0.2
+}
+precioFinal = precioOriginal*cantidad*(1-descuento)
+alert(`FACTURA\nArticulo: ${nombreArticulo}\nCantidad : ${cantidad} \nPrecio Unidad: $${precioOriginal}\nDescuento: ${descuento*100}%\nEl precio final es $${precioFinal}`)
+
+
+
+
+
+
+let presupuestoAnual=+ prompt("Ingrese el presupuesto anual para el hospital"),psiquiatria =+prompt("Ingrese el porcentaje asgnado a Psiquiatría"),pediatria =+prompt("Ingrese el porcentaje asgnado a Pediatría"),traumatologia =+prompt("Ingrese el porcentaje asgnado a Traumatología")
+if(psiquiatria+pediatria+traumatologia == 100)
+{
+    let pPsiquiatria = psiquiatria/100*presupuestoAnual
+    let pPediatria = pediatria/100*presupuestoAnual
+    let pTraumatologia = traumatologia/100*presupuestoAnual
+    alert(`Porcentaje Psiquiatría: ${psiquiatria}%\nValor asignado: $${pPsiquiatria}\nPorcentaje Pediatría: ${pediatria}%\nValor asignado: $${pPediatria}\nPorcentaje Traumatología: ${traumatologia}%\nValor asignado: $${pTraumatologia}`)
+}
+else{
+    alert(`Los porcentajes asignados no corresponden al 100%`)
+}
+
+
+
+
+
+let precioKM = 2.5, distancia =+prompt("Ingrese la distancia del viaje"),dias =+ prompt("Ingrese los dias de estancia"),precioTiquete = precioKM*distancia*2
+if(dias>=7 && distancia >800){
+precioTiquete *=0.7
+}
+alert(`El precio del tiquete de ida y vuelta es $${precioTiquete}`)
